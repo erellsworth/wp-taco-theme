87aaf677b8842ca190d367a30f35743412525878
Edward <graphicartgeek@gmail.com>
2018-03-10 19:40:55 -0700

    base files

---
+++ footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage WP_Taco_Theme
+ * @since 1.0
+ * @version 1.2
+ */
+
+	wp_footer(); ?>
+</div><!-- #tortilla -->
+
+</body>
+</html>
