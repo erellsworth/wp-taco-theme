a726beb9d879d7e487c7349950c41d30c96e4bc2
Edward <edward.ellsworth@gmail.com>
2018-03-11 18:47:37 -0700

    typography

--- functions.php
+++ functions.php
@@ -23,7 +23,7 @@ function wp_taco_scripts(){
 	//fonts
 	wp_enqueue_style( 'font_awesome', 'https://use.fontawesome.com/releases/v5.0.8/css/all.css' );
 	//see https://fontawesome.com for usage
-	wp_enqueue_style( 'google_fonts', 'https://fonts.googleapis.com/css?family=Amaranth:400,400i,700,700i|Lobster+Two:400,400i,700,700i' );
+	wp_enqueue_style( 'google_fonts', 'https://fonts.googleapis.com/css?family=Lobster+Two:400,400i,700,700i|Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i' );
 	
 	//flex grid
 	//see http://flexboxgrid.com/ for usage
